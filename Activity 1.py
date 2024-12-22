768d68796f4a197e8a803fd8eb53a80408ff34e8
Avanthikacoding <aviedayilliam@gmail.com>
2024-12-22 12:38:59 +0000

    Add files via upload

---
+++ Activity 1.py
@@ -0,0 +1,36 @@
+x = 25
+y = 10
+if x is y:
+    print(x , "does hold the same value as" , y)
+else:
+    print(x , "does not hold the same value as" , y)
+
+a = "Avi"
+b = "v"
+if a is not b:
+    print(a , " and " , b , "do not match, unfortunately 😔")
+else:
+    print(a , " and " , b , "do match. Hooray 😜")
+
+i = "My name is Avanthika 🥰" 
+k = "🥰"
+if k in i:
+    print(k , "is in " , i , " Yes 😜" )
+else:
+    print(k, "is not in" , i , " Oh no 😱")
+
+n = "Hello! Life is great 😊"
+m = "😊"
+if m not in n:
+    print(m , "is not in" , n , "Oh dear 😔😔 Please fix it!" )
+else: 
+    print(m , "is in" , n , "Hooray 😊😜")
+
+o = 15
+z = 10
+print("o >> 1 (right shift) " , o >> 1)
+print("z << 1 (left shift)" , z << 1)
+
+
+
+
---
+++ Activity 2.py
@@ -0,0 +1,4 @@
+o = 15
+z = 10
+print("o >> 1 (right shift) " , o >> 1)
+print("z << 1 (left shift)" , z << 1)
